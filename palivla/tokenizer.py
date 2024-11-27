Fix masking

palivla/tokenizer.py
@@ -179,11 +179,17 @@ class Tokenizer:
             [tokens_by_name["prefix"], tokens_by_name["causal"], tokens_by_name["pad"]],
             axis=0,
         )[: self.config.max_pad_length]
+
         include_prefix_mask = (
             tf.ones_like(tokens_by_name["prefix"], dtype=tf.bool)
             if self.config.prompt_autoregressive
             else tf.zeros_like(tokens_by_name["prefix"], dtype=tf.bool)
         )
+        
+        include_prefix_always_mask = (
+            tf.ones_like(tokens_by_name["prefix"], dtype=tf.bool)
+        )
+
         mask_ar = tf.concat(
             [
                 include_prefix_mask,
@@ -192,6 +198,16 @@ class Tokenizer:
             ],
             axis=0,
         )[: self.config.max_pad_length]
+
+        mask_ar_generation = tf.concat(
+            [
+                include_prefix_always_mask,
+                tf.ones_like(tokens_by_name["causal"], dtype=tf.bool),
+                tf.ones_like(tokens_by_name["pad"], dtype=tf.bool),
+            ],
+            axis=0,
+        )[: self.config.max_pad_length]
+
         mask_loss = tf.concat(
             [
                 include_prefix_mask,
@@ -201,7 +217,16 @@ class Tokenizer:
             axis=0,
         )[: self.config.max_pad_length]
 
-        return tokens, mask_ar, mask_loss
+        mask_loss_generation_only = tf.concat(
+            [
+                include_prefix_always_mask,
+                tf.zeros_like(tokens_by_name["causal"], dtype=tf.bool),
+                tf.zeros_like(tokens_by_name["pad"], dtype=tf.bool),
+            ],
+            axis=0,
+        )[: self.config.max_pad_length]
+
+        return tokens, mask_ar, mask_loss, mask_ar_generation, mask_loss_generation_only
 
     def tokenize_language_instruction(self, data):
         instruction = data["task"]["language_instruction"]
@@ -244,12 +269,14 @@ class Tokenizer:
             + self.config.action_vocab_offset,
         }
 
-        tokens, mask_ar, mask_loss = self.compose_token_structure(tokens)
+        tokens, mask_ar, mask_loss, mask_ar_generation, mask_loss_generation_only = self.compose_token_structure(tokens)
 
         return {
             "tokens": tokens,
             "mask_ar": mask_ar,
             "mask_loss": mask_loss,
+            "mask_ar_generation": mask_ar_generation,
+            "mask_loss_generation_only": mask_loss_generation_only,
             "mask_input": tokens != self.config.pad_token,
         }
 
@@ -258,7 +285,7 @@ class Tokenizer:
             "prompt": language_token_instructions[: self.config.max_pad_length - 10],
         }
 
-        tokens, mask_ar, mask_loss = self.compose_token_structure(
+        tokens, mask_ar, mask_loss, mask_ar_generation, mask_loss_generation_only = self.compose_token_structure(
             tokens, include_keys={"prefix", "pad"}
         )
 