e33aa5eeb6e7adf0956115bfa10b4ef7467e2418
Oier Mees <oier.mees@gmail.com>
2024-12-20 18:34:43 -0800

    add cloud bridge location

--- palivla/configs/bridge_config.py
+++ palivla/configs/bridge_config.py
@@ -32,7 +32,7 @@ def get_config():
             "dataset_kwargs": {
                 "oxe_kwargs": {
                     "data_mix": "bridge",
-                    "data_dir": "/data/rlds/",
+                    "data_dir": "gs://rail-orca-central2/resize_256_256",
                     "load_camera_views": ["primary"],
                     "load_depth": False,
                     "load_proprio": True,
