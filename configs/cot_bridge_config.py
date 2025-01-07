a8b2dfe4a09bec688bf7088a5d72579664dfc454
Kyle Stachowicz <kylestach99@gmail.com>
2025-01-06 19:03:38 -0800

    Add CoT back in

--- configs/cot_bridge_config.py
+++ configs/cot_bridge_config.py
@@ -1,90 +1,25 @@
-from octo.data.utils.data_utils import NormalizationType
-from ml_collections.config_dict import placeholder, ConfigDict, FieldReference
+from ml_collections import ConfigDict, FieldReference
+from ml_collections.config_dict import placeholder
+from palivla.base_config import get_config as get_base_config
 
-from palivla.components.model import get_default_config
+def get_config(variant_config: str = "default"):
+    config = get_base_config(variant_config).to_dict()
+    config["sequence_builder"] = "sequence_builder.cot(prompt_pad_length=50, gen_pad_length=110)"
 
-placeholder(int)._value
+    config["cot_path"] = FieldReference(None, str)
 
+    config["dataset_kwargs"]["oxe_kwargs"]["use_cot"] = True
+    config["dataset_kwargs"]["oxe_kwargs"]["cot_data_path"] = config["cot_path"]
+    for v in config["visualization_datasets"].values():
+        v["use_cot"] = True
+        v["cot_data_path"] = config["cot_path"]
 
-def get_config():
-    num_train_steps = FieldReference(100000, int)
-
-    model_config = get_default_config()
-
-    return ConfigDict(
-        {
-            # W&B settings
-            "wandb_project": "palivla-bridge",
-            "wandb_mode": "online",
-            # Tokenizers
-            "language_tokenizer": "google/paligemma-3b-pt-224",
-            "action_tokenizer": "action_tokenizer.bin(min_action_value=-3, max_action_value=3)",
-            "sequence_builder": "sequence_builder.default(prompt_pad_length=50, gen_pad_length=10)",
-            # Initialization
-            "load_fns": [
-                (
-                    "load.paligemma_weights",
-                    {
-                        "hf_repo": "google/paligemma-3b-pt-224-jax",
-                        "path": "paligemma-3b-pt-224.npz",
-                    },
-                )
-            ],
-            "resume_checkpoint_dir": None,
-            "resume_checkpoint_step": None,
-            # Overfit the dataset (for smoke tests/debugging)
-            "overfit_dataset": False,
-            # Training settings
-            "batch_size": 192,
-            "eval_batch_size": 128,
-            "num_steps": num_train_steps,
-            # Checkpoint settings
-            "save_path": placeholder(str),
-            "save_interval": 10000,
-            "max_to_keep": 1,
-            # Logging and visualization
-            "eval_interval": 100,
-            "log_interval": 1,
-            # Multi-device settings
-            "data_axis_size": 1,
-            "fsdp_axis_size": -1,
-            # Model
-            "model_config": model_config,
-            # Optimizer settings
-            "optimizer": {
-                "name": "optimizer.default_optimizer",
-                "kwargs": {
-                    "optimizer": "sgd",
-                    "num_train_steps": num_train_steps,
-                    "base_learning_rate": 1e-4,
-                },
-            },
-            # Dataset settings
-            "dataset_kwargs": {
-                "oxe_kwargs": {
-                    "data_mix": "bridge",
-                    "data_dir": "/data/rlds/",
-                    "load_camera_views": ["primary"],
-                    "load_depth": False,
-                    "load_proprio": True,
-                    "load_language": True,
-                    "force_recompute_dataset_statistics": False,
-                    "action_proprio_normalization_type": NormalizationType.NORMAL,
-                    "use_cot": True,
-                    "cot_data_path":"gs://multi-robot-bucket2/data/generated_data",
-                },
-                "traj_transform_kwargs": {
-                    "window_size": 1,
-                    "action_horizon": 1,
-                },
-                "frame_transform_kwargs": {
-                    "image_augment_kwargs": {},
-                    "resize_size": {"primary": [224, 224]},
-                },
-                "balance_weights": True,
-                "shuffle_buffer_size": 50000,
-                "traj_transform_threads": 16,
-                "traj_read_threads": 16,
-            },
+    if variant_config == "smoke_test":
+        config["visualizations"] = {
+            "overfit_sanity_print": {
+                "dataset": "overfit",
+                "visualization": "viz.sanity_print",
+            }
         }
-    )
+
+    return ConfigDict(config)
---
+++ src/palivla/components/cot_sequence_builder.py
@@ -0,0 +1,100 @@
+from dataclasses import dataclass
+from os import PathLike
+
+import cloudpickle
+import einops
+import numpy as np
+import tensorflow as tf
+from transformers import AutoTokenizer
+
+from big_vision.utils import Registry
+from palivla.components.action_tokenizer import ActionTokenizer
+from palivla.components.sequence_builder import SequenceBuilder
+
+
+@Registry.register("sequence_builder.cot")
+@dataclass
+class CoTSequenceBuilder(SequenceBuilder):
+    def prepare_cot(self, reasonings):
+        if isinstance(reasonings, bytes):
+            reasonings = reasonings.decode("utf-8")
+
+        assert isinstance(reasonings, str)
+
+        return str(reasonings)
+
+    def prepare_gen(self, reasonings, action_tokens):
+        if isinstance(reasonings, bytes):
+            reasonings = reasonings.decode("utf-8")
+
+        assert isinstance(reasonings, str)
+
+        return (
+            str(reasonings) + "<begin_of_action>" + super().prepare_gen(action_tokens)
+        )
+
+    def build_sequence(
+        self,
+        batch,
+        language_tokenizer: AutoTokenizer,
+        action_tokenizer: ActionTokenizer,
+        begin_is_prompt: bool = False,
+        include_action_tokens: bool = True,
+    ):
+        boa_id = language_tokenizer.encode("<begin_of_action>")[0]
+        bor_id = language_tokenizer.encode("<begin_of_reasoning>")[0]
+
+        boa_prompt = "<begin_of_reasoning>" if begin_is_prompt else ""
+        boa_gen = "" if begin_is_prompt else "<begin_of_reasoning>"
+        prompt = [
+            self.prepare_prompt(instruction) + boa_prompt
+            for instruction in batch["task"]["language_instruction"]
+        ]
+
+        prompt_tokens = language_tokenizer.batch_encode_plus(prompt)["input_ids"]
+
+        if include_action_tokens:
+            action = batch["action"]
+            if action.ndim == 4:
+                action = action[..., -1, :, :]
+
+            gen_tokens = [
+                boa_gen + self.prepare_gen(reasonings, t)
+                for reasonings, t in zip(
+                    batch["reasonings"], action_tokenizer.tokenize(action)
+                )
+            ]
+            gen_tokens = language_tokenizer.batch_encode_plus(gen_tokens)["input_ids"]
+        else:
+            gen_tokens = [[] for _ in range(len(prompt_tokens))]
+
+        return {
+            "prompt": self.pad_and_format_token_group(
+                prompt_tokens,
+                self.prompt_pad_length,
+                is_gen=[tok == bor_id for tok in prompt_tokens],
+                is_loss=False,
+            ),
+            "gen": self.pad_and_format_token_group(
+                gen_tokens, self.gen_pad_length, is_gen=True, is_loss=True
+            ),
+        }
+
+    def get_chain_of_thought(
+        self,
+        tokens: np.ndarray,
+        language_tokenizer: AutoTokenizer,
+        *,
+        bor_id: int | None = None,
+    ):
+        if bor_id is None:
+            bor_id = language_tokenizer.encode("<begin_of_reasoning>")[0]
+
+        # Find the beginning of the action (end of CoT)
+        try:
+            end_idx = np.where(tokens == bor_id)[0][0]
+            tokens = tokens[:end_idx]
+        except IndexError:
+            tokens = tokens
+
+        return language_tokenizer.decode(tokens)
