4650634341e6e497ceeaf6fa23f7fe13981698c5
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-04-23 19:09:14 -0700

    Update

--- src/palivla/frodo_dataset.py
+++ src/palivla/frodo_dataset.py
@@ -103,7 +103,7 @@ def decode_video_frames_torchvision(
     video_path = video_path.split("frodo-bucket-c2/")[-1]
     blob = bucket.blob(video_path)
     os.makedirs("~/videos", exist_ok=True)
-    local_video_path = Path("~/videos") / video_path.split("/")[-1]
+    local_video_path = f"~/videos/{video_path.split("/")[-1]}"
     blob.download_to_filename(local_video_path)
     reader = torchvision.io.VideoReader(local_video_path, "video")
 
