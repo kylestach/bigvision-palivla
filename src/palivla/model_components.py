ae20c0487b460ceff4b0c139aba0d56bf3991194
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-04-24 11:59:20 -0700

    Update

--- src/palivla/model_components.py
+++ src/palivla/model_components.py
@@ -169,6 +169,8 @@ class ModelComponents:
             "prompt": sequences["prompt"],
             "gen": sequences["gen"],
         }
+
+        breakpoint()
         batch = self.sharding.mesh.local_data_to_global_array(batch)
 
         # Run the train step
