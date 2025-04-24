dc45b1b75ab96679badd3118f45176a78de92214
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-04-24 10:08:04 -0700

    Update

--- scripts/train_frodo.py
+++ scripts/train_frodo.py
@@ -316,10 +316,10 @@ def main(_):
         batch_size=config.batch_size,
         # sampler=train_ds.get_sampler(),
         shuffle = True,
-        # num_workers = 0,
+        num_workers = 0,
         num_workers=128,
         collate_fn=collate_fn,
-        multiprocessing_context='forkserver', # don't fork - jax gets mad 
+        # multiprocessing_context='forkserver', # don't fork - jax gets mad 
     )
     # breakpoint()
     # print("Making train it ")
