de86e49e7c52977513532e5422611a77b5aa9138
Catherine Glossop <catherine_glossop@berkeley.edu>
2025-04-24 10:17:51 -0700

    Update

--- scripts/train_frodo.py
+++ scripts/train_frodo.py
@@ -317,7 +317,7 @@ def main(_):
         # sampler=train_ds.get_sampler(),
         shuffle = True,
         num_workers = 0,
-        num_workers=128,
+        # num_workers=128,
         collate_fn=collate_fn,
         # multiprocessing_context='forkserver', # don't fork - jax gets mad 
     )
